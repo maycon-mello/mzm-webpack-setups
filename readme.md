88c0fa134d4bc09cd06585d4dc948e492a0be028
Maycon Mello <maycon.mello@objectedge.com>
2018-03-29 23:31:50 -0300

    Add support for eslint and flowtype

--- readme.md
+++ readme.md
@@ -7,10 +7,10 @@ There are other branches on this repository to:
 - Webpack 4 + React + Redux
 - Webpack 4 + React + Relay Modern (with local graphql schema)
 
-
-All these setups include support for:
-- css, scss
+All these setups includes:
+- css, scss loaders
 - react router (the exemple project uses dynamic imports for routes)
 - css extract plugin
 - html webpack plugin
-
+- eslint
+- flowtype
--- src/pages/account/Account.js
+++ src/pages/account/Account.js
@@ -1,12 +1,30 @@
+/* @flow */
 import React from 'react';
-
 import './Account.scss';
 
+type AccountState = {
+  count: number,
+};
+
 export default class Account extends React.Component {
+
+  state: AccountState = {
+    count: 0,
+  };
+
+  inc = () => this.setState({
+    count: this.state.count + 1,     
+  });
+
   render() {
+    const value = this.props.value;
+
     return (
       <div className="page-account">
-        Account page
+        <div>
+          Account page {this.state.count}
+        </div>
+        <button onClick={this.inc}>Increment</button>
       </div>
     );
   }
